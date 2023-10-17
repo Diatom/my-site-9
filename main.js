292ff0ad5ec29d621faaf64a81f214fe6ac22051
Diatom <Supriore@gmail.com>
2023-10-17 17:20:50 +0300

    update

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 import { listofcheese } from './cheese.js';
 
-// class Templ {
-//   constructor(che) {
-//     this.che = che;
-//     }
-//   displayCheese() {
-//     document.getElementById(`bodymain`).appendChild(this.che);
-//   }
-// }
-
-function createList() {
-  for (const property in listofcheese) {
-      const obj = document.createElement(`div`);
-      for (const property in listofcheese) {
-       const item1 = document.createElement(`p`);
-        item1.innerHTML = `${property}: ${listofcheese[property]}`;
-        obj.appendChild(item1);
-      }
-      console.log(obj);
-      return obj;
+for (const property in listofcheese) {
+  if (listofcheese.hasOwnProperty(property)) {
+  const obj = document.createElement(`div`);
+  
+  for (const prop in listofcheese[property]) {
+    if (listofcheese[property].hasOwnProperty(prop)) {
+      const item = document.createElement(`p`);
+      item.innerHTML = prop + ": " + listofcheese[property][prop];
+      obj.appendChild(item);
+    }
+  }
+  
+  document.body.appendChild(obj);
   }
 }
-document.getElementById(`bodymain`).appendChild(createList());
 
 
-// const templ = new Templ(cheese);
-// templ.displayCheese();
+// function createList() {
+//   for (const property in listofcheese) {
+//     const cheese = document.createElement(`div`);
+//     if (listofcheese.hasOwnProperty(property)) {
+//       for (const prop in listofcheese[property]) {
+//         if (listofcheese[property].hasOwnProperty(prop)) {
+//           const item = document.createElement(`p`);
+//           item.innerHTML = prop + `: ` + listofcheese[property][prop];
+//           cheese.appendChild(item);
+//         }
+//       }
+//       return cheese;
+//     }
+//   }
+// }
+// document.body.appendChild(createList());
